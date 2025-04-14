df807e3799d6a6dd3e8fd407a04f9dd135d739bb
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-14 15:25:45 +0300

    Add examples of loops and functions with while loop fix and function variations

--- app.py
+++ app.py
@@ -107,8 +107,45 @@ for item in t2:
 t = (1, 2, 2, 3, 4)
 
 print(t.count(2))     # How many times 2 appears → 2
-print(t.index(3))     # First index of 3 → 3
+print(t.index(3))     
 
-##t[0] = 99  # ❌ TypeError — tuples are immutable!
+##FUNCTIONS
+def greetings(name):
+    return (f"Hello {name} welcome to my first python function after a very longtime")
+print(greetings("JOHN"))
+def Checking(number):
+    if number==4:
+        print("its 4 guys")
+    elif number==5:
+        print("I knew it people")
+    else:
+        print("Its just a random number who even cares")
+Checking(4)
+Checking(5)
+Checking(14)
+
+
+def add(a, b):
+    return a + b
+
+result = add(3, 5)
+print(result)  
+
+for num in range(1,5):
+    print(num)
+
+for num in range(5):
+    if num == 3:
+        break     # exit the loop
+    print(num)
+
+for num in range(5):
+    if num == 3:
+        continue   # skip 3
+    print(num)
+i=0
+while i<4:
+    print(f"I love you  python")
+    i+=1
 
 
