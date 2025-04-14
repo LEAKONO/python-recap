8a221e73c0ca6ee368329bf8a676c43ef142e031
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-14 12:57:57 +0300

    Added basic variable declarations and demonstrated data types: int, str, bool, float, list, tuple, set, and dict.

---
+++ app.py
@@ -0,0 +1,33 @@
+x=4
+print(x)
+print(type(x))
+y="Hello"
+print(type(y))
+
+print(y)
+z=True
+print(z)
+print(type(z))
+
+T=4.5
+print(type(T))
+
+print(T)
+arr=[1,2,3,4,5,6]
+print(arr)
+print(type(arr))
+
+Tu=(1,2,3,5,6)
+print(Tu)
+print(type(Tu))
+
+Se={1,5,6,7,8,0}
+print(type(Se))
+
+Di={
+    "name":"Mercedz",
+    "age":12
+}
+print(type(Di))
+print(Di)
+
